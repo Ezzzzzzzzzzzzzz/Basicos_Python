beed876224e5701c61121a70dafed7bf208036cd
Ezzzzzzzzzzzzzz <ezzzzer@gmail.com>
2022-09-23 12:15:46 -0500

    Programas .py

---
+++ Variables/Variables.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+x = 5
+y = 1 + x*5
+z = ("Hola a todos")
+w = input("Ingresa tu color favorito: ")
+edad = int(input("Ingresa tu edad: "))
+peso = float(input("Cuanto pesas?: "))
+
+#Imprime los datos
+print("El valor de x es: ", x)
+print("El valor de y es: ", y)
+print("Tu color favorito es ", w, ", tienes: ", edad, "años y pesas", peso, "kg")
